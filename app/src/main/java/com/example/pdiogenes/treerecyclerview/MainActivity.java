870402a089e85714e7428d073c33b599c073efb4
Pedro Diógenes <pedroyolo@live.com>
2019-04-29 05:54:31 -0300

    return to parent function + fragment function added

--- app/src/main/java/com/example/pdiogenes/treerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/pdiogenes/treerecyclerview/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.pdiogenes.treerecyclerview;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.media.Image;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,13 +20,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements RecyclerInterface {
+public class MainActivity extends AppCompatActivity implements RecyclerInterface, leafContent.OnFragmentInteractionListener {
 
     public RecyclerView rvNodes;
     LinearLayoutManager llm;
     NodeAdapter adapter;
-    private List<Node> nodeList = new ArrayList<Node>();
+    private Node raiz = new Node("raiz", "raiz", null);
+    //private List<Node> nodeList = new ArrayList<Node>();
+    private List<Node> nodeList;
     public ImageButton btnAddExterno;
+    public ImageButton btnReturn;
     public String nodeContent = "";
 
 
@@ -30,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerInterface
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nodeList = raiz.getChildren();
 
         /* Setting up the Recycler View */
         rvNodes = findViewById(R.id.rvNodes);
@@ -47,20 +55,62 @@ public class MainActivity extends AppCompatActivity implements RecyclerInterface
             }
         });
 
+        btnReturn = findViewById(R.id.btnParent);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToParent();
+            }
+        });
+
     }
 
     /* setting up the external node addition */
     public void externalNodePrep(){
         int currentNodes = nodeList.size();
-        String externalNodeID = Integer.toString(currentNodes + 1);
+        String externalNodeID;
+        if(nodeList == raiz.getChildren()){
+            externalNodeID = Integer.toString(currentNodes + 1);
+        }
+        else{
+            String parentId = nodeList.get(0).getParent().getIdNode();
+            externalNodeID = parentId + "." + (currentNodes+1);
+        }
         throwContentDialog(externalNodeID);
     }
 
     public void addExternalNode(String externalNodeID, String content){
-        Node externalNode = new Node(externalNodeID, content, null);
+        Node externalNode;
+        if(nodeList == raiz.getChildren()){
+            externalNode = new Node(externalNodeID, content, raiz);
+        }
+        else{
+            Node parent = nodeList.get(0).getParent();
+            externalNode = new Node(externalNodeID, content, parent);
+        }
         nodeList.add(externalNode);
         adapter.notifyDataSetChanged();
         Toast.makeText(this, "Node added", Toast.LENGTH_SHORT).show();
+    }
+
+    public void returnToParent(){
+        if(!nodeList.isEmpty()){
+            Node atualQualquer = nodeList.get(0);
+            Node parentOfParent = atualQualquer.getParent().getParent();
+
+            if(parentOfParent == null || nodeList.isEmpty()){
+                Toast.makeText(this, "Already at root", Toast.LENGTH_SHORT).show();
+            } else {
+                nodeList = parentOfParent.getChildren();
+                adapter = new NodeAdapter(this, nodeList, this);
+                //rvNodes.swapAdapter(adapter, true);
+                rvNodes.setAdapter(adapter);
+            }
+        }
+        else{
+            Toast.makeText(this, "There are no nodes", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     /* setting up the content alert dialog */
@@ -75,7 +125,10 @@ public class MainActivity extends AppCompatActivity implements RecyclerInterface
         contentDialog.setPositiveButton("Add node", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 nodeContent = txtContent.getText().toString();
-                addExternalNode(externalNodeID, nodeContent);
+                if(nodeContent.equals("")){
+                    addExternalNode(externalNodeID, "Não há conteudo");
+                }
+                else addExternalNode(externalNodeID, nodeContent);
             }
         });
 
@@ -88,12 +141,24 @@ public class MainActivity extends AppCompatActivity implements RecyclerInterface
         contentDialog.show();
     }
 
+    public void showFragment(String nodeID, String nodeContent){
+        leafContent leaf = leafContent.newInstance(nodeID, nodeContent);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right,
+                R.anim.enter_from_right, R.anim.exit_to_right);
+        transaction.addToBackStack(null);
+        transaction.add(R.id.fragmentContainer, leaf, "LEAF").commit();
+    }
+
 
     @Override
     public void onItemClick(Object object){
         Node clickedNode = (Node) object;
         if(clickedNode.isLeaf()){
-            // add fragment code
+            String id = clickedNode.getIdNode();
+            String content = clickedNode.getContent();
+            showFragment(id, content);
         }
         else{
             nodeList = clickedNode.getChildren();
@@ -101,5 +166,10 @@ public class MainActivity extends AppCompatActivity implements RecyclerInterface
             //rvNodes.swapAdapter(adapter, true);
             rvNodes.setAdapter(adapter);
         }
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+        onBackPressed();
     }
 }
